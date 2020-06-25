6cc763e4a3a559c4c7ae081855e7f275374e6b48
Xu <562592292@qq.com>
2020-06-25 11:56:46 +0800

    数据库存储SQL仓库 建立  (未测试缓存安全性)

--- cucund-core/cucund-db/src/main/java/com/cucund/project/db/provider/SelectProviderImpl.java
+++ cucund-core/cucund-db/src/main/java/com/cucund/project/db/provider/SelectProviderImpl.java
@@ -1,33 +1,29 @@
 package com.cucund.project.db.provider;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.cucund.project.db.dao.DBSqlDao;
 import com.cucund.project.db.entity.DBSql;
-import com.cucund.project.tool.utils.spring.SpringUtil;
+import com.cucund.project.db.entity.NullSql;
+import com.cucund.project.db.store.SqlStore;
+import com.cucund.project.tool.utils.exception.SystemError;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Map;
 
+@Slf4j
 public class SelectProviderImpl {
 
     public String selectOne4DbSql(@Param("sqlCode") String sqlCode, @Param("data") Map<String,Object> data) {
-        DBSqlDao dbSqlDao = SpringUtil.getBean(DBSqlDao.class);
-        DBSql dbSql = dbSqlDao.selectOne(
-            new QueryWrapper<DBSql>().lambda()
-                .eq(DBSql::getCode,sqlCode)
-                .eq(DBSql::getType,"selectOne")
-        );
+        DBSql dbSql = SqlStore.getInstance().get(sqlCode, "selectOne");
+        if(dbSql instanceof NullSql)
+            SystemError.throwException("未检测到数据操作语句");
         return dbSql.getSqlScript();
     }
 
     public String selectList4DbSql(@Param("sqlCode") String sqlCode, @Param("data") Map<String,Object> data) {
-        DBSqlDao dbSqlDao = SpringUtil.getBean(DBSqlDao.class);
-        DBSql dbSql = dbSqlDao.selectOne(
-                new QueryWrapper<DBSql>().lambda()
-                        .eq(DBSql::getCode,sqlCode)
-                        .eq(DBSql::getType,"selectList")
-        );
+        DBSql dbSql = SqlStore.getInstance().get(sqlCode, "selectList");
+        if(dbSql instanceof NullSql)
+            SystemError.throwException("未检测到数据操作语句");
         return dbSql.getSqlScript();
     }
 }
