814c1f3487c89aa043d4781e529ea1db078a7fa5
Xu <562592292@qq.com>
2020-07-06 11:36:20 +0800

    README.md   补充

--- cucund-mvc/cucund-test/src/main/java/com/cucund/project/test/controller/TestController.java
+++ cucund-mvc/cucund-test/src/main/java/com/cucund/project/test/controller/TestController.java
@@ -3,9 +3,7 @@ package com.cucund.project.test.controller;
 import com.cucund.project.spring.ob.InvokeClass;
 import com.cucund.project.spring.ob.ObEvent;
 import com.cucund.project.spring.ob.Observer;
-import com.cucund.project.test.proto.UserProto;
 import com.cucund.project.test.service.TestService;
-import com.google.protobuf.InvalidProtocolBufferException;
 import com.project.cucund.redis.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,19 +52,5 @@ public class TestController {
 
     @PostConstruct
     public void init() throws ClassNotFoundException {log.info("TestController PostConstruct 方法执行");}
-
-    @GetMapping("proto")
-    public byte[] getProto(){
-        UserProto.User.Builder builder = UserProto.User.newBuilder();
-        UserProto.User person = builder.setName("123").setAge(10).build();
-        byte[] bytes = person.toByteArray();
-        return bytes;
-    }
-
-    @GetMapping("reproto")
-    public String getProto(byte[] data) throws InvalidProtocolBufferException {
-        UserProto.User user = UserProto.User.parseFrom(data);
-        return user.toString();
-    }
 
 }
